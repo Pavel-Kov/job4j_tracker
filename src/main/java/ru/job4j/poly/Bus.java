298b51060bd1dd74587ef082fc633bd667355f1d
Pavel <wangle@bk.ru>
2021-11-29 16:06:59 +0300

    Исправление 3. Интерфейс Input [#181092 #233042]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,16 +4,16 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
-
+        System.out.println("Автобус выехал");
     }
 
     @Override
     public void passengers(int passengers) {
-
+        System.out.println("Кол-во пассажиров: " + passengers);
     }
 
     @Override
     public double refueling(int liters) {
-        return 0;
+        return liters * 50;
     }
 }
