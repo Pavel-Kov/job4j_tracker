422d737b46614b6e7e486e72bf57ddc39fc27f98
Pavel <wangle@bk.ru>
2022-02-09 11:38:16 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("test1@test.ru", "Ivanov");
+        map.put("test2@test.ru", "Petrov");
+        map.put("test3@test.ru", "Sidorov");
+
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
