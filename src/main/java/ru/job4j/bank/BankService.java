5c1d9224a4f986c1e407e52f6b3d66352ac3719f
Pavel <wangle@bk.ru>
2022-04-01 14:41:32 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,14 +5,32 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс описывает действия банковских сервисов
+ * @author Pavel
+ * @version 1.0
+ */
 public class BankService {
 
+    /**
+     * Данные клиента храняться в структуре ключ-значение
+     * Ключ клиент, значение - список аккаунтов клиента
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Мотод добавляет клиента банка и создает пустой список аккаунтов
+     * @param user клиент
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<Account>());
     }
 
+    /**
+     * Метод ищет клиента и добавляет аккаунт
+     * @param passport номер паспорта клиента
+     * @param account аккаунта клиента
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null && !users.get(user).contains(account)) {
@@ -20,6 +38,11 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод ищет клиента по номеру паспорта
+     * @param passport номер паспорта
+     * @return возвращает клиента
+     */
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
             if (passport.equals(user.getPassport())) {
@@ -29,6 +52,12 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод ищет аккаунт по номеру паспорта и реквизиту
+     * @param passport номер паспорта клиента
+     * @param requisite реквизит аккаунта
+     * @return возвращает аккаунт
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -41,6 +70,15 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод переводит средства с аккаунта а аккаунт, если их достаточно.
+     * @param srcPassport номер паспорта отправляющего клиента
+     * @param srcRequisite реквизит отправляющего аккаунта
+     * @param destPassport номер паспорта принимающего клиента
+     * @param destRequisite реквизит принимающего аккаунта
+     * @param amount размер перевода
+     * @return возвращает выполен перевод или нет
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
